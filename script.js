fe44d91d3947e1e668c1a0d8b5dda25b6ec3598d
akashsfw <akashsingh.nsps26@gmail.com>
2026-08-29 15:22:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -184,8 +184,7 @@ function clearanceCode(seed) {
 
 
 
-resetBtn.addEventListener('click', () => {
-  trapArmed  = true;               
+resetBtn.addEventListener('click', () => {            
   dodgeTally = 0;
   sneakyBtn.classList.remove('tamed');
   btnFace.textContent  = 'Submit application';
